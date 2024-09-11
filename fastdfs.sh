224016e37f9276200cdfa554424a629afa1fa20f
不忘初心 <1624717079@qq.com>
2024-09-11 11:48:24 +0800

    移除centos更换为debian

--- fastdfs.sh
+++ fastdfs.sh
@@ -12,12 +12,12 @@ cp /opt/storage.conf /etc/fdfs/storage.conf
 cp /opt/client.conf /etc/fdfs/client.conf
 cp /opt/mod_fastdfs.conf /etc/fdfs/mod_fastdfs.conf
 
-sed -i -e "s|###TRACKER###|$TRACKER_SERVER|g" /etc/fdfs/client.conf
-sed -i -e "s|###TRACKER###|$TRACKER_SERVER|g" /etc/fdfs/storage.conf
-sed -i -e "s|###TRACKER###|$TRACKER_SERVER|g" /etc/fdfs/mod_fastdfs.conf
-sed -i -e "s|###RESERVED_STORAGE_SPACE###|$RESERVED_STORAGE_SPACE|g" /etc/fdfs/tracker.conf
+sed -i -e "s|###TRACKER###|${TRACKER_SERVER}|g" /etc/fdfs/client.conf
+sed -i -e "s|###TRACKER###|${TRACKER_SERVER}|g" /etc/fdfs/storage.conf
+sed -i -e "s|###TRACKER###|${TRACKER_SERVER}|g" /etc/fdfs/mod_fastdfs.conf
+sed -i -e "s|###RESERVED_STORAGE_SPACE###|${RESERVED_STORAGE_SPACE}|g" /etc/fdfs/tracker.conf
 
-echo ${FASTDFS_MODE}
+echo "${FASTDFS_MODE}"
 
 if [ "${FASTDFS_MODE}" = "tracker" ] ;
     then
